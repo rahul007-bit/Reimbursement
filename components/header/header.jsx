36a25880d2675419de417b74fc06bcceda9e6c81
Mayank Viramgama <mayankviramgama5@gmail.com>
2022-07-26 22:42:02 +0530

    setup tailwind

---
+++ components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className='bg-red-200'>Header</header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header/header';
+import Form from '../components/form/form';
+
+const Login = () => {
+    return (
+        <div>
+            <Header/>
+            <Form/>
+        </div>
+    );
+};
+
+export default Login;
