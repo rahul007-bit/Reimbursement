ae853e06effb3b8b7de14c483fa64d63e44e6adc
rahul007-bit <yadavrahul8141@yahoo.com>
2022-07-30 17:06:18 +0530

    done some formatting

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className='bg-424483 h:100px w-screen'>
-            <header >Header</header>
-        </div>
-    );
+  return (
+    <div className="bg-424483 h:100px w-screen">
+      <header>Header</header>
+    </div>
+  );
 };
 
 export default Header;
--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from '../components/header/header';
-import Form from '../components/form/form';
+import React from "react";
+import Header from "../components/header/header";
+import Form from "../components/form/form";
 
 const Login = () => {
-    return (
-        <div>
-            {/* <Header/> */}
-            <Form/>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Form />
+    </div>
+  );
 };
 
 export default Login;
