af47ed56235a2d9fbb0b983fa2ae45183f83b92c
Mansi <mviramgama1@gmail.com>
2022-09-01 13:44:55 +0530

    Nptel form

--- components/reimbursementForm/CretificationForm/nptelform.jsx
+++ components/reimbursementForm/CretificationForm/nptelform.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Stack, Box, TextField } from "@mui/material";
+import { Stack, Box, TextField, Typography } from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
 
 const NptelForm = () => {
   return (
@@ -7,41 +8,88 @@ const NptelForm = () => {
       <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
         <TextField
           fullWidth
-          label="Course Name"
+          label="Student Name"
           id="outlined-required"
           required
         ></TextField>
         <TextField
           fullWidth
-          label="email"
-          id="outlined-required"
-          required
-        ></TextField>
-      </Stack>
-      <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
-        <TextField
-          fullWidth
-          label="Course Name"
-          id="outlined-required"
-          required
-        ></TextField>
-        <TextField
-          fullWidth
-          label="email"
+          label="Email"
           id="outlined-required"
           required
         ></TextField>
       </Stack>
+
       <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
         <TextField
-          fullWidth
+          sx={{ width: 255 }}
           label="Course Name"
           id="outlined-required"
           required
         ></TextField>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={[{ label: "First Half" }, { label: "Second half" }]}
+          sx={{ width: 260 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Academic Year" />
+          )}
+        />
+      </Stack>
+
+      <Typography variant="h8" gutterBottom>
+        Duration
+      </Typography>
+      <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
         <TextField
           fullWidth
-          label="email"
+          label="Start Date"
+          id="outlined-required"
+          type="date"
+          // defaultValue="2020-05-26"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          required
+        ></TextField>
+        <TextField
+          fullWidth
+          label="End Date"
+          id="outlined-required"
+          type="date"
+          // defaultValue="2020-05-26"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          required
+        ></TextField>
+        <TextField
+          fullWidth
+          label="Exam Date"
+          id="outlined-required"
+          type="date"
+          // defaultValue="2020-05-26"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          required
+        ></TextField>
+      </Stack>
+      <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={[{ label: "Elite" }, { label: "Gold" }, { label: "Silver" }]}
+          sx={{ width: 250 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Class Received(if any)" />
+          )}
+        />
+
+        <TextField
+          sx={{ width: 250 }}
+          label="Reimbursement Amount"
           id="outlined-required"
           required
         ></TextField>
--- components/reimbursementForm/ReimbursementForm.jsx
+++ components/reimbursementForm/ReimbursementForm.jsx
@@ -31,13 +31,13 @@ const ReimbursementForm = ({ formData }) => {
                   <Autocomplete
                     disablePortal
                     id="combo-box-demo"
-                    options={[{ label: "Student" }, { label: "Staff" }]}
+                    options={[{ label: "Student" }, { label: "NPTEL" }]}
                     renderInput={(params) => (
-                      <TextField {...params} label="Movie" />
+                      <TextField {...params} label="Select Role" />
                     )}
                     onInputChange={getUserDetails}
                   />
-                  <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
+                  {/* <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
                     <TextField
                       fullWidth
                       label="Student Name"
@@ -50,7 +50,78 @@ const ReimbursementForm = ({ formData }) => {
                       id="outlined-required"
                       required
                     ></TextField>
+                    
+                  
+
                   </Stack>
+                  <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
+                    <TextField
+                      sx={{width:255}}
+                      label="Course Name"
+                      id="outlined-required"
+                      required
+                    ></TextField>
+                     <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={[{ label: "First Half" }, { label: "Second half" }]}
+                    sx={{ width: 260 }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Academic Year" />
+                    )}
+                
+                  />
+                    
+                    </Stack>
+                  
+                    <Typography variant="h8" gutterBottom>
+        Duration
+      </Typography>
+      <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
+                    <TextField
+                      fullWidth
+                      label="Start Date"
+                      id="outlined-required"
+                      required
+                    ></TextField>
+                    <TextField
+                      fullWidth
+                      label="End Date"
+                      id="outlined-required"
+                      required
+                    ></TextField>
+                     <TextField
+                      fullWidth
+                      label="Exam Date"
+                      id="outlined-required"
+                      required
+                    ></TextField>
+                    
+                    
+                  
+
+                  </Stack>
+                  <Stack direction={{ sm: "column", md: "row" }} spacing={3}>
+                  <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={[{ label: "Elite" }, { label: "Gold" },{label: "Silver"}]}
+                    sx={{ width: 250 }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Class Received(if any)" />
+                    )}
+                
+                  />
+                   
+                   <TextField
+                      sx={{width:250}}
+                      label="Reimbursement Amount"
+                      id="outlined-required"
+                      required
+                    ></TextField>
+                    
+                  </Stack> */}
+
                   {user === "NPTEL" && <NptelForm />}
                 </Stack>
               </Box>
