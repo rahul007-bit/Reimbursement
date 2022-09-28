9b87278921cc0e355fa1f40d2c145529a8305ed5
rahul007-bit <yadavrahul8141@yahoo.com>
2022-09-28 14:23:15 +0530

    fix validation failed error

--- server/services/user/requestValidator.js
+++ server/services/user/requestValidator.js
@@ -14,6 +14,7 @@ const requestValidator = {
       lastName: Joi.string(),
       moodleId: Joi.string().required(),
       password: Joi.string().required(),
+      type: Joi.string().required(),
     }),
   },
 };
